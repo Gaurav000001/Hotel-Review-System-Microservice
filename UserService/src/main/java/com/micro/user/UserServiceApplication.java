7cde685c06b7678d4302787fe338d5b97b6228a5
Gaurav <gangulygaurav166@gmail.com>
2023-09-18 00:07:08 +0530

    added required moditions to register the service with ServiceRestry using spring-cloud-netflix-eureka-client

--- UserService/src/main/java/com/micro/user/UserServiceApplication.java
+++ UserService/src/main/java/com/micro/user/UserServiceApplication.java
@@ -3,6 +3,11 @@ package com.micro.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@EnableEurekaClient is deprecated, no need to annotate the main class. 
+//It is enough to add the spring-cloud-starter-netflix-eureka-client dependency 
+//to pom.xml and if we have the application name in yml or properties file it 
+//will be registered to Eureka Server.
+
 @SpringBootApplication
 public class UserServiceApplication {
 
